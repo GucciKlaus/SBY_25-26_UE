84c5a726fff2de17ed64cbb2d41e66dfde7d37e8
Klaus <klaus.hoehfurtner@gmail.com>
2026-01-23 21:01:40 +0100

    Beispiel mit Prozesses besser kommentiert

--- prog.c
+++ prog.c
@@ -7,21 +7,23 @@
 int main(int argc, char **argv, char **envp)
 {
     printf("Hallo von prog1 PID=%d \n", getpid());
+    int pid2 = getpid();
     pid_t pid = fork();
     if (pid == 0)
     {
+        printf("Kind mit ID %d von ElternPID=%d\n", getpid(),pid2);
         char *argv2[] = {"irgendwas", 0};
         if (execve("prog2", argv2, envp) == -1)
         {
             printf("Error starting prog2\n");
             return -1;
         }
-        sleep(5);
-        printf("Kind von PID=%d\n", getpid());
+        //sleep(5);
+       
     }
     else
     {
-        sleep(10);
+        //sleep(10);
         wait(0);
         printf("Eltern: Baba von prog1 mit PID=%d\n", getpid());
     }
